235f61446e1154fa79756716cb4434df2b732931
William Antonio <william.fatecsjc@gmail.com>
2016-06-17 15:06:27 -0300

    updating again the strcuture

---
+++ bpms603/bpms603-restore-timer/src/main/java/com/redhat/gss/brms/restore/MigrationApp.java
@@ -0,0 +1,9 @@
+package com.redhat.gss.brms.restore;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class MigrationApp extends Application {
+
+}
